57b50e1906d01023fbb5f4147d0d0e0009e8e65c
ChucklesDroid <mj.akarsh@gmail.com>
2022-07-11 19:45:12 +0530

    added edit functionality to forms

--- ChatNow/base/views.py
+++ ChatNow/base/views.py
@@ -27,3 +27,15 @@ def createRoom(request):
     context = { 'form':form }
     return render(request, "base/createRoom.html", context)
 
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = Form(instance=room)
+
+    if request.method == "POST":
+        form = Form(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {'form': form}
+    return render(request, 'base/createRoom.html', context)
