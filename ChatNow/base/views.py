fcc009d154c8e404c6e15f79b6cce807052c62de
ChucklesDroid <mj.akarsh@gmail.com>
2022-07-14 01:48:09 +0530

    added search functionality

--- ChatNow/base/views.py
+++ ChatNow/base/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db import models
+from django.db.models import Q
 from .models import Room, Topic, Messages
 from .forms import Form
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+
+    context = {'rooms':rooms, 'topics':topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
