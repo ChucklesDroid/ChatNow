0dd9dc4872889781eeade4118193d12b3ab31930
ChucklesDroid <mj.akarsh@gmail.com>
2022-07-18 18:15:40 +0530

    added logout functionality

--- ChatNow/base/views.py
+++ ChatNow/base/views.py
@@ -31,6 +31,10 @@ def loginPage(request):
     context = {}
     return render(request, 'base/login_register.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 def home(request):
 
